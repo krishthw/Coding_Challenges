e23f8bf2f86c4b5b47e02504f149d7e94de9f352
Krish W <53475147+krishthw@users.noreply.github.com>
2022-08-22 12:12:38 -0400

    Update morse_code_decoder.py

--- morse_code_decoder.py
+++ morse_code_decoder.py
@@ -1,5 +1,12 @@
 # This is a code to decode morse code messagers. 
 # In morse code message each letter is seperated by space and each word is seperated by two spaces.
+
+# My approach : 
+# will be translating letter by letter. 
+# if track a code(not a space), append it to a string.
+# if track a space increase the counter by one, and map the code using the dictionary.
+# if the counter == 2, add a space to the decoded message.
+
 M= { 'A':'.-', 'B':'-...',
                     'C':'-.-.', 'D':'-..', 'E':'.',
                     'F':'..-.', 'G':'--.', 'H':'....',
@@ -36,3 +43,4 @@ for line in sys.stdin:
                 text ='' # set text to blank string
     print(decode)
 
+# P.S need to add a extra space initially to the encoded message as counter needs to be 1 to go to the loop which decode the message
